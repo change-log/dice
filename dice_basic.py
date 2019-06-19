5701aea108ee664ecba6bcbc6d95c04f478fc3db
change-log <52012151+change-log@users.noreply.github.com>
2019-06-19 21:10:22 +0100

    Delete dice_basic.py

--- dice_basic.py
+++
@@ -1,9 +0,0 @@
-print("Dice Roll")
-
-x = input("Press Enter to roll"          
-
-if x in("")                               
-  print(random.randrange(1,6))     
-          
-else                                      
-  print("Invalid Response)               
